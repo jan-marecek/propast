d51421410034e25e22937b1eb8b3d0840d9de116
JanMarecek <janmarecek@yahoo.com>
2024-11-15 18:31:07 +0100

    header classes and section component

---
+++ src/lib/components/layouts/Section.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let sectionClass: string;
+  export let id: string;
+</script>
+
+<section {id} class="{sectionClass} mx-auto max-w-content px-mobile">
+  <slot />
+</section>
