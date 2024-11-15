ac7593a0cb88bf16fde3d1883dc14da9c631c198
JanMarecek <janmarecek@yahoo.com>
2024-11-15 20:58:08 +0100

    fix: hero image gradiant

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -1,17 +1,17 @@
 <script>
   import Image from "$lib/components/images/ImageFromVitePicture.svelte";
-  import Hero from "/src/assets/homepage/hero.jpg";
+  import Hero from "/src/assets/homepage/hero_l.jpg";
 </script>
 
 <section class="relative">
-  <div class="relative overflow-hidden bg-white py-56">
+  <div class="relative overflow-hidden bg-white py-80">
     <Image
       alt="Hero"
       class="absolute inset-0 h-full w-full object-cover"
       loading="eager"
       pictureData={Hero}
     />
-    <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary"></div>
+    <div class="absolute inset-0 bg-gradient-to-b from-primary to-transparent"></div>
     <div class="relative z-10 mx-auto flex max-w-content flex-col gap-8 px-mobile">
       <h1 class="h1">Propast</h1>
       <span class="text-neutral sm:text-xl lg:text-2xl">
