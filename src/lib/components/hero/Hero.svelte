8d17efb5b3bd961b90160155723b436d51bc8bf5
JanMarecek <janmarecek@yahoo.com>
2024-11-15 19:11:38 +0100

    wip hero

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -9,10 +9,10 @@
       loading="eager"
       pictureData=""
     /> -->
-    <div class="absolute inset-0 bg-gradient-to-r from-primary to-secondary"></div>
+    <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary"></div>
     <div class="relative z-10 mx-auto flex max-w-content flex-col gap-8 px-mobile">
       <h1 class="h1">Propast</h1>
-      <span class=" sm:text-xl lg:text-2xl">
+      <span class="text-neutral sm:text-xl lg:text-2xl">
         Portál Rozborů a Odborných Podkladů, Archa Scénických Technologií.</span
       >
     </div>
