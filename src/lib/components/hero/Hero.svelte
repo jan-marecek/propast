4e2d2cacecfe330dd26ac800ceead290e2bdfba3
JanMarecek <janmarecek@yahoo.com>
2024-11-15 22:19:29 +0100

    feat: hero page final

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section class="relative">
-  <div class="relative overflow-hidden bg-white py-80">
+  <div class="relative overflow-hidden bg-white py-64">
     <Image
       alt="Hero"
       class="absolute inset-0 hidden h-full w-full object-cover md:block"
@@ -15,8 +15,8 @@
       class="absolute inset-0 bg-gradient-to-b from-primary to-secondary md:to-transparent"
     ></div>
     <div class="relative z-10 mx-auto flex max-w-content flex-col gap-8 px-mobile">
-      <h1 class="h1">Propast</h1>
-      <span class="text-neutral sm:text-xl lg:text-2xl">
+      <h1 class="h1 text-base-300">Propast</h1>
+      <span class="text-base-300 sm:text-xl lg:text-2xl">
         Portál Rozborů a Odborných Podkladů, Archa Scénických Technologií.</span
       >
     </div>
