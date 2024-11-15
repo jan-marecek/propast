aa45953000546434b8b5d90447ffe9cdaffa2dd5
JanMarecek <janmarecek@yahoo.com>
2024-11-15 20:33:49 +0100

    wip: import hero picture

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -1,14 +1,16 @@
 <script>
+  import Image from "/src/lib/components/Image.svelte";
+  import Hero from "/src/assets/hp/hero_xl.jpg";
 </script>
 
 <section class="relative">
   <div class="relative overflow-hidden bg-white py-56">
-    <!-- <Image
+    <Image
       alt="Hero"
       class="absolute inset-0 h-full w-full object-cover"
       loading="eager"
-      pictureData=""
-    /> -->
+      pictureData={Hero}
+    />
     <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary"></div>
     <div class="relative z-10 mx-auto flex max-w-content flex-col gap-8 px-mobile">
       <h1 class="h1">Propast</h1>
