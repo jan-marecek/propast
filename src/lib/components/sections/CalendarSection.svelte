cdc5a0f86c26073a450da6df7b0ae4b27ddd65ac
JanMarecek <janmarecek@yahoo.com>
2025-02-07 20:28:38 +0100

    fix calendar roundness

--- src/lib/components/sections/CalendarSection.svelte
+++ src/lib/components/sections/CalendarSection.svelte
@@ -5,7 +5,7 @@
 <section id="calendar" class="relative w-full overflow-hidden bg-primary px-mobile pb-14 lg:pb-32">
   <div class="relative">
     <h2 class="h1 px-mobile py-14 text-center text-base-300 lg:pb-20 lg:pt-32">Kalendář</h2>
-    <div class="mx-auto max-w-content overflow-x-auto rounded-3xl bg-white px-3 md:px-12">
+    <div class="mx-auto max-w-content overflow-x-auto rounded-sm bg-white px-3 md:px-12">
       <div class="">
         <!-- Adjust the min-width as needed -->
         <Calendar />
